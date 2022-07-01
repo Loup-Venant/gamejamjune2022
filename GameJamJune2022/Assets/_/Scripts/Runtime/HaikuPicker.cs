dae32d559e865c2d86b83a271de2f07745593384
Liador <jean.lousberg@yahoo.fr>
2022-07-01 15:45:31 +0200

    added haiku to end scene

--- GameJamJune2022/Assets/_/Scripts/Runtime/HaikuPicker.cs
+++ GameJamJune2022/Assets/_/Scripts/Runtime/HaikuPicker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Gameplay.Data;
+using TMPro;
 
 namespace Gameplay.Runtime
 {
@@ -15,11 +16,13 @@ namespace Gameplay.Runtime
     }
     private void Start()
     {
-      Debug.Log(GetHaiku(true));
-      Debug.Log(GetHaiku(true));
-      Debug.Log(GetHaiku(true));
-      Debug.Log(GetHaiku(false));
-      Debug.Log(GetHaiku(false));
+      var tmp = GetComponent<TextMeshProUGUI>();
+      tmp.text = GetHaiku(true) +"\n\n\n" + tmp.text;
+      //Debug.Log(GetHaiku(true));
+      //Debug.Log(GetHaiku(true));
+      //Debug.Log(GetHaiku(true));
+      //Debug.Log(GetHaiku(false));
+      //Debug.Log(GetHaiku(false));
     }
   }
 }
