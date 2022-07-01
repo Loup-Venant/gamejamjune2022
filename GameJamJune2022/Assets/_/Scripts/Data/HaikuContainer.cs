c0b04c2f05faeb83cd59e16cae449da5325d1054
Liador <jean.lousberg@yahoo.fr>
2022-07-01 12:20:13 +0200

    finished Haiku creator

--- GameJamJune2022/Assets/_/Scripts/Data/HaikuContainer.cs
+++ GameJamJune2022/Assets/_/Scripts/Data/HaikuContainer.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 namespace Gameplay.Data
 {
+  [CreateAssetMenu(menuName = "Gameplay/Haiku")]
   public class HaikuContainer : ScriptableObject
   {
     public List<string> HaikuParts = new List<string>();
     public string HaikuEndWin;
     public string HaikuEndLoss;
-    public List<string> randomPart;
+    public List<string> randomPart =new List<string>();
 
     public string GetHaiku(bool win)
     {
-      var index = Random.Range(0, HaikuParts.Count);
-      var retVal = HaikuParts[index];
-      HaikuParts.RemoveAt(index);
-      while (HaikuParts.Count > 0)
+      var list = new List<string>();
+      list.AddRange(HaikuParts);
+      if (randomPart.Count > 0)
+        list.Add(randomPart[Random.Range(0, randomPart.Count)]);
+      var index = Random.Range(0, list.Count);
+      var retVal = list[index];
+      list.RemoveAt(index);
+      while (list.Count > 0)
       {
-        retVal += "\n" + HaikuParts[index];
-        HaikuParts.RemoveAt(index);
+        index = Random.Range(0, list.Count);
+        retVal += "\n" + list[index];
+        list.RemoveAt(index);
       }
       retVal += "\n";
       retVal += win ? HaikuEndWin : HaikuEndLoss;
